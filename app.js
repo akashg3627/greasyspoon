fbcbf6419e692f5487afeef3eee3ecf6122193ec
Dipin <dgargdipin@gmail.com>
2020-09-01 12:38:02 +0530

    a

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(expressCspHeader({
     directives: {
-        'default-src': [SELF, '*.google.com', 'https://apis.google.com/js/'],
+        'default-src': [SELF, '*.google.com', '*://*.*.com/*', 'https://apis.google.com/js/'],
         'script-src': [SELF, INLINE, 'apis.google.com', 'https://apis.google.com/js/api.js'],
     }
 }));
